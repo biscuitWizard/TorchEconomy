3db5a5618a5c840bd2678dbcc72a671576b7a8e6
asdfasdfa <ddffdsa@afdasda.com>
2019-06-07 15:55:33 -0700

    Initial commit of source.

---
+++ TorchEconomySE/EconomyPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using Torch;
+using Torch.API.Plugins;
+using Torch.Views;
+
+namespace TorchEconomySE
+{
+    public class EconomyPlugin : TorchPluginBase, IWpfPlugin
+    {
+        public EconomyConfiguration Config => _config?.Data;
+        private Persistent<EconomyConfiguration> _config;
+
+        private UserControl _control;
+
+        /// <inheritdoc />
+        public UserControl GetControl() => _control ?? (_control = new PropertyGrid() { DataContext = Config/*, IsEnabled = false*/});
+    }
+}
