397931053ced48084a693ea5631f7226c4a3fa9f
asdfasdfa <ddffdsa@afdasda.com>
2019-06-15 09:52:31 -0700

    Adding missing protomember market for intermod API file.

--- TorchEconomy/Messages/Accounts/AdjustBalanceMessage.cs
+++ TorchEconomy/Messages/Accounts/AdjustBalanceMessage.cs
@@ -10,20 +10,24 @@ namespace TorchEconomy.Messages.Accounts
 		/// <summary>
 		/// Target account ID to modify.
 		/// </summary>
+		[ProtoMember(201)]
 		public long AccountId { get; set; }
 		/// <summary>
 		/// The amount to adjust by.
 		/// </summary>
+		[ProtoMember(202)]
 		public decimal Amount { get; set; }
 		/// <summary>
 		/// Reason for adjusting balance.
 		/// </summary>
+		[ProtoMember(203)]
 		public string Reason { get; set; }
 		
 		/// <summary>
 		/// Optional field. If specified, will also deduct from the specified accountID and
 		/// add their ID to the transaction log.
 		/// </summary>
+		[ProtoMember(204)]
 		public long? FromAccountId { get; set; }
 	}
 
