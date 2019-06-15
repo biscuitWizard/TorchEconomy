2897ef4f3347c1afd36a9e58682a58052faa7872
asdfasdfa <ddffdsa@afdasda.com>
2019-06-15 13:15:35 -0700

    ???

--- TorchEcomony.Markets/Commands/MarketModule.cs
+++ TorchEcomony.Markets/Commands/MarketModule.cs
@@ -1,97 +1,15 @@
-﻿using System;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
+using System;
 using System.Text;
-using NLog;
-using Sandbox.Game.Entities;
 using Torch.Commands;
-using TorchEconomy;
 using TorchEconomy.Markets.Data.Models;
 using TorchEconomy.Markets.Managers;
 using VRage.Game;
-using VRage.ModAPI;
 
 namespace TorchEconomy.Markets.Commands
 {
-    [Category("econ markets")]
+    [Category("econ")]
     public class MarketModule : EconomyCommandModule
     {
-        private static readonly Logger Log = LogManager.GetLogger("Economy.Commands.TradeZone");
-//
-//        [Command("zones", "Lists all trade zones player is currently in.")]
-//        public void GetZones()
-//        {
-//            var tradeZoneManager = EconomyPlugin.GetManager<TradeZoneManager>();
-//
-//            var playerPosition = Context.Player.Character.GetPosition();
-//            var tradeZones = tradeZoneManager.GetTradeZonesInRange(playerPosition).ToArray();
-//            if (tradeZones.Length == 0)
-//            {
-//                Context.Respond("There are no trade zones in range of you.");
-//                return;
-//            }
-//            
-//            var stringBuilder = new StringBuilder("Nearby Trade Zones:");
-//            foreach (var tradeZone in tradeZones)
-//            {
-//                stringBuilder.AppendLine($"[{tradeZone.Name}] {tradeZone.Position.DistanceFrom(playerPosition)}m");
-//            }
-//
-//            Context.Respond(stringBuilder.ToString());
-//        }
-
-        [Command("create", "<stationGridName> <newMarketName>: Creates a market using the specified station grid and names it based on the new market name.")]
-        public void CreateMarket(string stationGridName, string marketName)
-        {
-            var character = Context.Player.Character;
-            if (character == null)
-            {
-                Context.Respond("You are dead. You cant trade ships while dead.");
-                return;
-            }
-
-            MyCubeGrid stationEntity = null;
-            if (!Utilities.TryGetEntityByNameOrId(stationGridName, out IMyEntity entity))
-            {
-                Context.Respond($"Unable to find a station by the name of '{stationGridName}'.");
-                return;
-            }
-            
-            stationEntity = entity as MyCubeGrid;
-            if (stationEntity == null
-                || !stationEntity.IsStatic)
-            {
-                Context.Respond($"Unable to find a station by the name of '{stationGridName}'.");
-                return;
-            }
-
-            var manager = EconomyPlugin.GetManager<MarketManager>();
-            
-            // Check we have ownership.
-            if (!stationEntity.BigOwners.Contains(Context.Player.IdentityId))
-            {
-                Context.Respond("You must own a majority of the grid to be able authorize a station as a market.");
-                return;
-            }
-            
-            // Market checks to see if the station is already registered.
-            manager.GetMarkets()
-                .Then(markets =>
-                {
-                    Log.Info("Received markets");
-                    if (markets.Any(m => m.ParentGridId == entity.EntityId))
-                    {
-                        Context.Respond("This station is already marked as a market.");
-                        return;
-                    }
-
-                    manager.CreateMarket(entity.EntityId, Context.Player.SteamUserId, marketName,
-                            EconomyMarketsPlugin.Instance.Config.DefaultMarketRange)
-                        .Then(newMarket => { Context.Respond($"{marketName} has been successfully established."); })
-                        .Catch(error => { Context.Respond($"[ERROR] Unable to create market: {error.Message}"); });
-                }).Catch(error => Log.Error(error));
-        }
-
         [Command("list", "Lists available goods to buy.")]
         public void List()
         {
@@ -107,7 +25,7 @@ namespace TorchEconomy.Markets.Commands
 
             Context.Respond(stringBuilder.ToString());
         }
-
+        
         [Command("buy", "<itemNameOrIndex> <quantity>: Purchases a quantity of items from nearby tradezones at the lowest prices available.")]
         public void Buy(string itemNameOrIndex, decimal quantity)
         {
