e8803fe898542b3e8324273d2a7f88328f2e89ea
Julie Iaccarino <1904063+biscuitWizard@users.noreply.github.com>
2019-06-18 03:07:36 -0700

    adding four new market admin commands.

--- TorchEcomony.Markets/Commands/AdminModule.cs
+++ TorchEcomony.Markets/Commands/AdminModule.cs
@@ -6,6 +6,8 @@ using Torch.Commands;
 using Torch.Commands.Permissions;
 using TorchEconomy.Data.Types;
 using TorchEconomy.Managers;
+using TorchEconomy.Markets.Data.DataObjects;
+using TorchEconomy.Markets.Data.Types;
 using TorchEconomy.Markets.Managers;
 using TorchEconomy.Resources;
 using VRage.Game.ModAPI;
@@ -87,6 +89,146 @@ namespace TorchEconomy.Commands
                                         })
                                         .Catch(HandleError);
                                 })
+                                .Catch(HandleError);
+                        })
+                        .Catch(HandleError);
+                })
+                .Catch(HandleError);
+        }
+        
+        [Command("buy.quantity", "<marketNameOrId> <itemName> <newQuantity>: Overrides a buy order quantity on a market.")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void SetBuyOrderQuantity(string marketNameOrId, string itemName, decimal newQuantity)
+        {
+            UpdateOrderQuantity(BuyOrderType.Buy, marketNameOrId, itemName, newQuantity);
+        }
+
+        [Command("sell.quantity", "<marketNameOrId> <itemName> <newQuantity>: Overrides a sell order quantity on a market.")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void SetSellOrderQuantity(string marketNameOrId, string itemName, decimal newQuantity)
+        {
+            UpdateOrderQuantity(BuyOrderType.Sell, marketNameOrId, itemName, newQuantity);
+        }
+        
+        [Command("buy.price", "<marketNameOrId> <itemName> <newQuantity>: Overrides a buy order price on a market.")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void SetBuyOrderPrice(string marketNameOrId, string itemName, decimal newPrice)
+        {
+            UpdateOrderPrice(BuyOrderType.Buy, marketNameOrId, itemName, newPrice);
+        }
+
+        [Command("sell.price", "<marketNameOrId> <itemName> <newQuantity>: Overrides a sell order price on a market.")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void SetSellOrderPrice(string marketNameOrId, string itemName, decimal newPrice)
+        {
+            UpdateOrderPrice(BuyOrderType.Sell, marketNameOrId, itemName, newPrice);
+        }
+        
+        private void UpdateOrderQuantity(BuyOrderType orderType, string marketNameOrId, string itemName, decimal newQuantity)
+        {
+            var character = Context.Player.Character;
+            if (character == null)
+            {
+                Context.Respond("You cannot do this while dead.");
+                return;
+            }
+            
+            if (!DefinitionResolver.TryGetDefinitionByName(itemName, out var itemDefinition))
+            {
+                Context.Respond($"Unable to find an item by the name of {itemName}");
+                return;
+            }
+
+            if (newQuantity < new decimal(0.01))
+            {
+                Context.Respond($"{newQuantity} is too small. Please use a higher value.");
+                return;
+            }
+            
+            var manager = EconomyPlugin.GetManager<MarketManager>();
+            var orderManager = EconomyPlugin.GetManager<MarketOrderManager>();
+            manager.GetMarkets()
+                .Then(markets =>
+                {
+                    MarketDataObject market = null;
+                    if (long.TryParse(marketNameOrId, out var marketId))
+                    {
+                        market = markets.FirstOrDefault(m => m.Id == marketId);
+                    }
+                    else
+                    {
+                        market = markets.FirstOrDefault(m =>
+                            m.Name.Equals(marketNameOrId, StringComparison.InvariantCultureIgnoreCase));
+                    }
+                    
+                    if (market == null)
+                    {
+                        Context.Respond($"Unable to find market by name or id of '{marketNameOrId}'.");
+                        return;
+                    }
+                    
+                    orderManager.GetMarketOrder(orderType, market.Id, itemDefinition.Id)
+                        .Then(order =>
+                        {
+                            orderManager.UpdateOrderQuantity(order.Id, newQuantity)
+                                .Then(() => Context.Respond(
+                                    $"Successfully updated Order#{order.Id}'s quantity to {newQuantity}."))
+                                .Catch(HandleError);
+                        })
+                        .Catch(HandleError);
+                })
+                .Catch(HandleError);
+        }
+        
+        private void UpdateOrderPrice(BuyOrderType orderType, string marketNameOrId, string itemName, decimal newPrice)
+        {
+            var character = Context.Player.Character;
+            if (character == null)
+            {
+                Context.Respond("You cannot do this while dead.");
+                return;
+            }
+            
+            if (!DefinitionResolver.TryGetDefinitionByName(itemName, out var itemDefinition))
+            {
+                Context.Respond($"Unable to find an item by the name of {itemName}");
+                return;
+            }
+
+            if (newPrice < new decimal(0.01))
+            {
+                Context.Respond($"{newPrice} is too small. Please use a higher value.");
+                return;
+            }
+            
+            var manager = EconomyPlugin.GetManager<MarketManager>();
+            var orderManager = EconomyPlugin.GetManager<MarketOrderManager>();
+            manager.GetMarkets()
+                .Then(markets =>
+                {
+                    MarketDataObject market = null;
+                    if (long.TryParse(marketNameOrId, out var marketId))
+                    {
+                        market = markets.FirstOrDefault(m => m.Id == marketId);
+                    }
+                    else
+                    {
+                        market = markets.FirstOrDefault(m =>
+                            m.Name.Equals(marketNameOrId, StringComparison.InvariantCultureIgnoreCase));
+                    }
+                    
+                    if (market == null)
+                    {
+                        Context.Respond($"Unable to find market by name or id of '{marketNameOrId}'.");
+                        return;
+                    }
+                    
+                    orderManager.GetMarketOrder(orderType, market.Id, itemDefinition.Id)
+                        .Then(order =>
+                        {
+                            orderManager.UpdateOrderPrice(order.Id, newPrice)
+                                .Then(() => Context.Respond(
+                                    $"Successfully updated Order#{order.Id}'s price to {Utilities.FriendlyFormatCurrency(newPrice)}."))
                                 .Catch(HandleError);
                         })
                         .Catch(HandleError);
