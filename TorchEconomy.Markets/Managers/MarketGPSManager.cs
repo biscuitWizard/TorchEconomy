2e57b21fd1346a48624cf9ebe0834d3dbc69e8ba
Julie Iaccarino <1904063+biscuitWizard@users.noreply.github.com>
2019-06-20 03:21:15 -0700

    Refactored MarketGPSManager to remove the hit to the database every second.

--- TorchEconomy.Markets/Managers/MarketGPSManager.cs
+++ TorchEconomy.Markets/Managers/MarketGPSManager.cs
@@ -10,6 +10,7 @@ using Sandbox.ModAPI;
 using TorchEconomy.Data;
 using TorchEconomy.Markets.Data.DataObjects;
 using VRage.Game.ModAPI;
+using VRage.ModAPI;
 using VRageMath;
 
 namespace TorchEconomy.Markets.Managers
@@ -33,6 +34,8 @@ namespace TorchEconomy.Markets.Managers
         private readonly MarketManager _marketManager;
         private readonly Dictionary<long, List<MarketGPS>> _playerMarketsInRange 
             = new Dictionary<long, List<MarketGPS>>();
+        private readonly Dictionary<MarketDataObject, MarketGPS> _cachedMarkets 
+            = new Dictionary<MarketDataObject, MarketGPS>();
         
         public MarketGPSManager(IConnectionFactory connectionFactory, MarketManager marketManager) 
             : base(connectionFactory)
@@ -43,9 +46,77 @@ namespace TorchEconomy.Markets.Managers
 
         public override void Start()
         {
+            LoadMarkets();
+            
             _processing = true;
             UpdateGps();
             _lastUpdate = DateTime.UtcNow;
+        }
+
+        private void LoadMarkets()
+        {
+            _marketManager
+                .GetMarkets()
+                .Then(markets =>
+                {
+                    foreach (var market in markets)
+                    {
+                        var marketEntity = MyAPIGateway.Entities.GetEntityById(market.ParentGridId) as MyCubeGrid;
+                        if (marketEntity == null)
+                            continue;
+
+                        var marketPosition = ((IMyEntity) marketEntity).GetPosition();
+                        var marketGps = new MarketGPS
+                        {
+                            Gps = CreateGPS(market, marketPosition),
+                            MarketRange = market.Range,
+                            MarketId = market.Id,
+                            Position = marketPosition
+                        };
+
+                        _cachedMarkets[market] = marketGps;
+                        marketEntity.OnStaticChanged += MarketEntityOnOnStaticChanged;
+                    }
+                });
+        }
+
+        private void MarketEntityOnOnStaticChanged(MyCubeGrid cubeGrid, bool newValue)
+        {
+            if (newValue)
+            {
+                _marketManager.GetMarketByGridId(cubeGrid.EntityId)
+                    .Then(market =>
+                    {
+                        var marketPosition = ((IMyEntity) cubeGrid).GetPosition();
+                        var marketGps = new MarketGPS
+                        {
+                            Gps = CreateGPS(market.Key, market.Value),
+                            MarketRange = market.Key.Range,
+                            MarketId = market.Key.Id,
+                            Position = marketPosition
+                        };
+
+                        _cachedMarkets[market] = marketGps;
+                    });
+            }
+            else
+            {
+                // Remove GPS points for this market for players that have it. Remove it from the cache.
+                var market = _cachedMarkets.Keys.FirstOrDefault(m => m.ParentGridId == cubeGrid.EntityId);
+                if (market == null)
+                    return;
+                foreach (var playerId in _playerMarketsInRange.Keys)
+                {
+                    var marketsInRangeForPlayer = _playerMarketsInRange[playerId];
+                    var marketGps = marketsInRangeForPlayer.FirstOrDefault(m => m.MarketId == market.Id);
+                    if (marketGps != null)
+                    {
+                        marketsInRangeForPlayer.Remove(marketGps);
+                        MyAPIGateway.Session?.GPS.RemoveGps(playerId, marketGps.Gps);
+                    }
+                }
+                _cachedMarkets.Remove(market);
+            }
         }
 
         public override void Update()
@@ -71,71 +142,47 @@ namespace TorchEconomy.Markets.Managers
         {
             return new Promise((resolve, reject) =>
             {
-                _marketManager
-                    .GetMarkets()
-                    .Then(markets =>
+                var players = new List<IMyPlayer>();
+                MyAPIGateway.Players.GetPlayers(players);
+
+                foreach (var player in players)
+                {
+                    var playerPosition = player.GetPosition();
+                    var marketsInRange = _cachedMarkets.Where(kvp =>
+                            Utilities.DistanceFrom(kvp.Value.Position, playerPosition) < kvp.Key.Range)
+                        .ToArray();
+
+                    if (!_playerMarketsInRange.TryGetValue(player.IdentityId, out var playerGps))
                     {
-                        var marketPositions = new Dictionary<MarketDataObject, Vector3D>();
-                        foreach (var market in markets)
-                        {
-                            var marketEntity = MyAPIGateway.Entities.GetEntityById(market.ParentGridId);
-                            if (marketEntity == null)
-                                continue;
+                        playerGps = new List<MarketGPS>();
+                        _playerMarketsInRange[player.IdentityId] = playerGps;
+                    }
 
-                            marketPositions[market] = marketEntity.GetPosition();
-                        }
-                        
-                        var players = new List<IMyPlayer>();
-                        MyAPIGateway.Players.GetPlayers(players);
-                        
-                        foreach (var player in players)
-                        {
-                            var playerPosition = player.GetPosition();
-                            var marketsInRange = marketPositions.Where(kvp =>
-                                Utilities.DistanceFrom(kvp.Value, playerPosition) < kvp.Key.Range)
-                                .ToArray();
+                    // Check if any GPS are now removed.
+                    foreach (var gps in playerGps.ToArray())
+                    {
+                        if (Utilities.DistanceFrom(gps.Position, playerPosition) <= gps.MarketRange)
+                            continue;
+                        playerGps.Remove(gps);
+                        MyAPIGateway.Session?.GPS.RemoveGps(player.Identity.IdentityId, gps.Gps);
+                    }
 
-                            if (!_playerMarketsInRange.TryGetValue(player.IdentityId, out var playerGps))
-                            {
-                                playerGps = new List<MarketGPS>();
-                                _playerMarketsInRange[player.IdentityId] = playerGps;
-                            }
-                            
-                            // Check if any GPS are now removed.
-                            foreach (var gps in playerGps.ToArray())
-                            {
-                                if (Utilities.DistanceFrom(gps.Position, playerPosition) <= gps.MarketRange)
-                                    continue;
-                                playerGps.Remove(gps);
-                                MyAPIGateway.Session?.GPS.RemoveGps(player.Identity.IdentityId, gps.Gps);
-                            }
-                            
-                            // Add any GPS which are new.
-                            foreach (var market in marketsInRange)
-                            {
-                                if (playerGps.Any(p => p.MarketId == market.Key.Id))
-                                    continue;
-                                
-                                // Add a new GPS.
-                                var marketGps = new MarketGPS
-                                {
-                                    Gps = CreateGPS(market.Key, market.Value),
-                                    MarketRange = market.Key.Range,
-                                    MarketId = market.Key.Id,
-                                    Position = marketPositions[market.Key]
-                                };
-                                
-                                // Render it for the player.
-                                MyAPIGateway.Session?.GPS.AddGps(player.Identity.IdentityId, marketGps.Gps);
-                                
-                                // Track it locally.
-                                playerGps.Add(marketGps);
-                            }
-                        }
+                    // Add any GPS which are new.
+                    foreach (var market in marketsInRange)
+                    {
+                        if (playerGps.Any(p => p.MarketId == market.Key.Id))
+                            continue;
 
-                        _processing = false;
-                        resolve();
-                    });
+                        // Render it for the player.
+                        MyAPIGateway.Session?.GPS.AddGps(player.Identity.IdentityId, market.Value.Gps);
+
+                        // Track it locally.
+                        playerGps.Add(market.Value);
+                    }
+                }
+
+                _processing = false;
+                resolve();
             });
         }
 
@@ -166,6 +213,7 @@ namespace TorchEconomy.Markets.Managers
             }
 
             _playerMarketsInRange.Clear();
+            _cachedMarkets.Clear();
         }
     }
 }
